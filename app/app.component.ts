8b3da3c9d12bf5ad88550d53f57de57f62a8d796
PratikPihulkar <pratikpihulkar2000@gmail.com>
2025-02-25 12:07:57 +0530

    mangalpande

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+zk.sdjbf avhskdcbd vAS}d
+cachesasdc
+satisfiesadc
+AudioScheduledSourceNodeas
+TextDecoderStream
+assda
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'Assignment1';
-}
+addEventListener
+asdas
+DataTransferad
